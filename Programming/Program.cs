07e48cba3c2dcd5e940e0f903803122b8945c59d
YASHsingh06 <syash9457@gmail.com>
2026-01-19 12:06:42 +0530

    Add new file without removing existing project

---
+++ Programming/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int m = int.Parse(input[0]);
+        int n = int.Parse(input[1]);
+
+        int luckyCount = 0;
+
+        for (int num = m; num <= n; num++)
+        {
+            if (!IsPrime(num))
+            {
+                int sumNum = SumOfDigits(num);
+                int sumSquare = SumOfDigits(num * num);
+
+                if (sumSquare == sumNum * sumNum)
+                {
+                    luckyCount++;
+                }
+            }
+        }
+
+        Console.WriteLine(luckyCount);
+    }
+
+    static int SumOfDigits(int number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
