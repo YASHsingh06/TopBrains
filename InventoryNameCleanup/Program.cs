654224c296bfe79875dc1546694b5946f3a4e403
YASHsingh06 <syash9457@gmail.com>
2026-01-20 12:31:38 +0530

    Added InventoryNameCleanup console project

---
+++ InventoryNameCleanup/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string input = " llapppptop bag ";
+
+
+        string cleaned = Regex.Replace(input.Trim(), @"\s+", " ");
+
+
+        StringBuilder resultBuilder = new StringBuilder();
+        char previousChar = '\0';
+
+        foreach (char currentChar in cleaned)
+        {
+            if (currentChar != previousChar)
+            {
+                resultBuilder.Append(currentChar);
+                previousChar = currentChar;
+            }
+        }
+
+
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        string finalResult = textInfo.ToTitleCase(resultBuilder.ToString().ToLower());
+
+        Console.WriteLine(finalResult);
+    }
+}
