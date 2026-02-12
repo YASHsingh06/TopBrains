9785728efe7368b1a37e9e08a2ab689b13b9abe0
YASHsingh06 <119647446+YASHsingh06@users.noreply.github.com>
2026-02-12 16:19:14 +0530

    Add files via upload

---
+++ Conversion.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution
+{
+    public static double FeetToCentimeters(int feet)
+    {
+        double centimeters = feet * 30.48;
+
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main(string[] args)
+    {
+        int feet = int.Parse(Console.ReadLine());
+
+        double result = FeetToCentimeters(feet);
+
+        Console.WriteLine(result);
+    }
+}
