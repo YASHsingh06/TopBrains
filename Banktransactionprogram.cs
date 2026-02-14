e3d0f7f1fc5b1b5642b1df4ae0c26c3b60258705
YASHsingh06 <119647446+YASHsingh06@users.noreply.github.com>
2026-02-14 10:04:08 +0530

    Add files via upload

---
+++ Banktransactionprogram.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution
+{
+    public int FinalBalance(int initialBalance, int[] transactions)
+    {
+        int balance = initialBalance;
+
+        foreach (int t in transactions)
+        {
+            if (t >= 0)
+            {
+                balance += t; 
+            }
+            else
+            {
+                if (balance + t >= 0)  
+                    balance += t;
+            }
+        }
+
+        return balance;
+    }
+}
